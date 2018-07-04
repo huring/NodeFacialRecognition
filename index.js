aebc9d8ce02246d5bde213bf4bd5529ba6fc1ab0
Lars Huring <lars.huring@gmail.com>
2018-07-04 15:34:37 +0200

    Extract faces from image

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ const fr = require('face-recognition')
 
 fr.winKillProcessOnExit()
 
-const detector = fr.AsyncFaceDetector()
+const detector = fr.FaceDetector()
 
-const lenna = fr.loadImage('./dataset/lars.1.jpg')
+const image = fr.loadImage('./dataset/IMG_2973.JPG')
 
 console.log('detecting faces')
 const faceSize = 150
-Promise.all([
-  detector.detectFaces(lenna, faceSize),
-])
-  .then(([lennaFaces]) => {
-    const win1 = new fr.ImageWindow()
-    win1.setImage(fr.tileImages(lennaFaces))
-    fr.hitEnterToContinue()
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+
+const faceImages = detector.detectFaces(image, faceSize)
+
+faceImages.forEach((img, i) => fr.saveImage(`./dataset/face_${i}.png`, img))
+
+
+// Promise.all([
+//   detector.detectFaces(lenna, faceSize),
+// ])
+//   .then(([lennaFaces]) => {
+//     const win1 = new fr.ImageWindow()
+//     win1.setImage(fr.tileImages(lennaFaces))
+//     fr.hitEnterToContinue()
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
